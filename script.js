5eea7b5b9ecbc0224e7e3ad7b79c830e4605f8cb
SrijanPatel <srijanpatel911@gmail.com>
2025-01-01 03:23:25 +0530

    Added more user interactivity

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let p1=document.querySelector("#p1");
 let player1=document.querySelector(".p1");
 let p2=document.querySelector("#p2");
 let player2=document.querySelector(".p2");
+let message=document.querySelector("#message");
+let msg=document.querySelector(".msg");
 let boxX=true;
 let count=-1;
 
@@ -12,6 +14,8 @@ let wp=[[0,1,2], [3,4,5],[6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 function winnerP() {
     for (win of wp){
         if (boxes[win[0]].innerHTML==="X" && boxes[win[1]].innerHTML==="X" && boxes[win[2]].innerHTML==="X"){
+            message.innerHTML=`Congratulations, You : WON`;
+            msg.style.display="flex";
             player1.style.border="2px solid white";
             player2.style.border="none";
 
@@ -20,8 +24,13 @@ function winnerP() {
 
         }
         if (boxes[win[0]].innerHTML==="O" && boxes[win[1]].innerHTML==="O" && boxes[win[2]].innerHTML==="O"){
+            message.innerHTML=`Congratulations, P2 : WON`;
+            msg.style.display="flex";
+
+
             player2.style.border="2px solid white";
             player1.style.border="none";
+
             p2.innerHTML=Number(p2.innerHTML) + 1;
             return true;
 
@@ -48,6 +57,11 @@ boxes.forEach(
     
             
             if ((winnerP()) || (count==8 && winnerP()!==true)){
+                if (count==8 && winnerP()!==true){
+                    message.innerHTML=`Oops, Match : TIED`;
+                    msg.style.display="flex";
+                }
+
                 count=-1;
                 boxes.forEach(
                     (box)=>{
